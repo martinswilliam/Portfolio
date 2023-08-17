8375080465508c0db4eaa2851916bb7f6b503b64
martinswilliam <williamgmca@gmail.com>
2023-08-17 03:41:15 -0300

    alterando SoftSkills

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,41 @@ function updateProfileInfo(profileData) {
    const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
+    
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
 
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+   
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}` 
+    
 }
+
+function updateSoftSkils(profileData) {
+    const softSkills = getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill =>`<li>${skill}</li>` ).join('')
+}
+
+
+
+
+
 
 
 (async () => {
 
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkils(profileData)
 
 })()
