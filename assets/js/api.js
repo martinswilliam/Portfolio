bdfdea3c1c7e7bb2f7cc21996135025e7de14ac5
martinswilliam <williamgmca@gmail.com>
2023-08-17 00:19:46 -0300

    Aterando Dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'http://raw.githibusercontent.com/martinswilliam/portifolio/main/dataprofile.js;';
+    const url = 'http://raw.githibusercontent.com/martinswilliam/portifolio/main/dataprofile.js';
     const fetching = await fetch(url)
     return await fetching.json()
 }
