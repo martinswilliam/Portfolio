b390a09261699de553a2a5aef55e41fa5705706a
martinswilliam <williamgmca@gmail.com>
2023-08-17 01:58:30 -0300

    atualizando dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'http://raw.githibusercontent.com/martinswilliam/portifolio/main/dataprofile.js';
+    const url = 'http://raw.githubusercontent.com/martinswilliam/Portfolio/blob/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
